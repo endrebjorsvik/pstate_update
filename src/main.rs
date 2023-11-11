2191b0e79258c1fd946a1291c2387ce5b3b5b9c0
Endre Bjørsvik <endrebjorsvik@gmail.com>
2023-11-11 01:45:47 +0100

    Add control of scaling governor, and use TOML file for configuring mapping over everything.

--- src/main.rs
+++ src/main.rs
@@ -33,25 +33,20 @@ impl fmt::Display for PPDPowerProfile {
     }
 }
 
-impl PPDPowerProfile {
-    /// Convert a PowerProfile to a matching EPP.
-    fn to_epp(&self) -> EnergyPerformancePreference {
-        match self {
-            PPDPowerProfile::PowerSaver => EnergyPerformancePreference::Power,
-            PPDPowerProfile::Balanced => EnergyPerformancePreference::BalancePower, // Or BalancePerformance?
-            PPDPowerProfile::Performance => EnergyPerformancePreference::Performance,
-        }
-    }
-}
-
+#[derive(serde::Deserialize)]
 /// Energy Performance Preference (EPP) exposed by the AMD PState driver
 enum EnergyPerformancePreference {
     #[allow(dead_code)]
+    #[serde(rename(deserialize = "default"))]
     Default,
+    #[serde(rename(deserialize = "performance"))]
     Performance,
     #[allow(dead_code)]
+    #[serde(rename(deserialize = "balance_performance"))]
     BalancePerformance,
+    #[serde(rename(deserialize = "balance_power"))]
     BalancePower,
+    #[serde(rename(deserialize = "power"))]
     Power,
 }
 
@@ -63,6 +58,49 @@ impl fmt::Display for EnergyPerformancePreference {
             EnergyPerformancePreference::BalancePerformance => write!(f, "balance_performance"),
             EnergyPerformancePreference::BalancePower => write!(f, "balance_power"),
             EnergyPerformancePreference::Power => write!(f, "power"),
+        }
+    }
+}
+
+impl FromStr for EnergyPerformancePreference {
+    type Err = String;
+    fn from_str(input: &str) -> Result<EnergyPerformancePreference, Self::Err> {
+        match input {
+            "default" => Ok(EnergyPerformancePreference::Default),
+            "performance" => Ok(EnergyPerformancePreference::Performance),
+            "balance_performance" => Ok(EnergyPerformancePreference::BalancePerformance),
+            "balance_power" => Ok(EnergyPerformancePreference::BalancePower),
+            "power" => Ok(EnergyPerformancePreference::Power),
+            _ => Err(format!("Could not parse {input}")),
+        }
+    }
+}
+
+/// Scaling governor exposed by the AMD Pstate driver
+#[derive(serde::Deserialize)]
+enum ScalingGovernor {
+    #[serde(rename(deserialize = "powersave"))]
+    PowerSave,
+    #[serde(rename(deserialize = "performance"))]
+    Performance,
+}
+
+impl fmt::Display for ScalingGovernor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ScalingGovernor::Performance => write!(f, "performance"),
+            ScalingGovernor::PowerSave => write!(f, "powersave"),
+        }
+    }
+}
+
+impl FromStr for ScalingGovernor {
+    type Err = String;
+    fn from_str(input: &str) -> Result<ScalingGovernor, Self::Err> {
+        match input {
+            "powersave" => Ok(ScalingGovernor::PowerSave),
+            "performance" => Ok(ScalingGovernor::Performance),
+            _ => Err(format!("Could not parse {input}")),
         }
     }
 }
@@ -79,7 +117,10 @@ trait PowerProfilesDaemonManager {
 
 /// EPPController controls the CPU EPP levels
 struct EPPController {
-    core_files: Vec<path::PathBuf>,
+    epp_core_files: Vec<path::PathBuf>,
+    epp_config: EPPConfig,
+    governor_core_files: Vec<path::PathBuf>,
+    governor_config: GovernorConfig,
 }
 
 impl EPPController {
@@ -96,9 +137,27 @@ impl EPPController {
     /// Write the provided EPP to all discovered CPU cores.
     fn write_epp_to_all_cores(&self, epp: &EnergyPerformancePreference) -> io::Result<()> {
         log::info!("Writing EPP {epp} to all EPP files.");
-        for f in self.core_files.iter() {
+        for f in self.epp_core_files.iter() {
             if let Err(e) = EPPController::write_epp_to_core(epp, f) {
                 log::error!("Failed to write EPP to core ({f:?}): {e}.")
+            }
+        }
+        Ok(())
+    }
+
+    /// Write the provided scaling governor to the CPU core given by the file path.
+    fn write_governor_to_core(gov: &ScalingGovernor, gov_file: &path::Path) -> io::Result<()> {
+        log::debug!("Writing governor '{gov}' to file {gov_file:?}.");
+        fs::write(gov_file, gov.to_string())?;
+        Ok(())
+    }
+
+    /// Write the provided EPP to all discovered CPU cores.
+    fn write_governor_to_all_cores(&self, gov: &ScalingGovernor) -> io::Result<()> {
+        log::info!("Writing governor {gov} to all governor files.");
+        for f in self.governor_core_files.iter() {
+            if let Err(e) = EPPController::write_governor_to_core(gov, f) {
+                log::error!("Failed to write governor to core ({f:?}): {e}.")
             }
         }
         Ok(())
@@ -138,8 +197,27 @@ impl EPPController {
             }
         };
         log::info!("ActiveProfile changed: {profile}");
-        self.write_epp_to_all_cores(&profile.to_epp())?;
+        self.write_epp_to_all_cores(self.desired_epp(&profile))?;
+        self.write_governor_to_all_cores(self.desired_governor(&profile))?;
         Ok(())
+    }
+
+    /// Select appropriate EPP from Power profile.
+    fn desired_epp(&self, profile: &PPDPowerProfile) -> &EnergyPerformancePreference {
+        match profile {
+            PPDPowerProfile::Performance => &self.epp_config.performance,
+            PPDPowerProfile::Balanced => &self.epp_config.balanced,
+            PPDPowerProfile::PowerSaver => &self.epp_config.power_saver,
+        }
+    }
+
+    /// Select appropriate Scaling Governor from Power profile.
+    fn desired_governor(&self, profile: &PPDPowerProfile) -> &ScalingGovernor {
+        match profile {
+            PPDPowerProfile::Performance => &self.governor_config.performance,
+            PPDPowerProfile::Balanced => &self.governor_config.balanced,
+            PPDPowerProfile::PowerSaver => &self.governor_config.power_saver,
+        }
     }
 }
 
@@ -171,7 +249,60 @@ fn find_cpu_core_epp_paths(cpufreq_path: &path::Path) -> Result<Vec<path::PathBu
     Ok(paths)
 }
 
+fn generate_cpu_core_gorvernor_paths(
+    epp_paths: &[path::PathBuf],
+) -> Result<Vec<path::PathBuf>, io::Error> {
+    let mut paths = Vec::new();
+    for epp in epp_paths {
+        let p = epp.with_file_name("scaling_governor");
+        if !p.exists() {
+            log::warn!("Governor file does not exist: {p:?}.");
+            continue;
+        }
+        paths.push(p);
+    }
+    log::info!("Found {} valid governor files.", paths.len());
+    Ok(paths)
+}
+
+#[derive(serde::Deserialize)]
+struct EPPConfig {
+    power_saver: EnergyPerformancePreference,
+    balanced: EnergyPerformancePreference,
+    performance: EnergyPerformancePreference,
+}
+
+#[derive(serde::Deserialize)]
+struct GovernorConfig {
+    power_saver: ScalingGovernor,
+    balanced: ScalingGovernor,
+    performance: ScalingGovernor,
+}
+
+#[derive(serde::Deserialize)]
+struct Config {
+    epp: EPPConfig,
+    scaling_governor: GovernorConfig,
+}
+
+fn read_config() -> Result<Config, io::Error> {
+    let mut config_file = path::Path::new("/etc/pstate_update/config.toml");
+    if !config_file.exists() {
+        log::warn!("Could not find {config_file:?}. Trying local folder instead.");
+        config_file = path::Path::new("config.toml");
+    }
+    let s = fs::read_to_string(config_file)?;
+    let config: Config = match toml::from_str(&s) {
+        Ok(c) => c,
+        Err(e) => {
+            return Err(io::Error::new(io::ErrorKind::Other, e));
+        }
+    };
+    Ok(config)
+}
+
 fn main() {
+    // TODO: Notify desktop on certain errors? Could be easily done using DBus.
     let env = env_logger::Env::new().default_filter_or("info");
     env_logger::init_from_env(env);
 
@@ -187,9 +318,30 @@ fn main() {
         log::error!("Could not find any valid EPP files. Exiting.");
         process::exit(1);
     }
+    let governor_files = match generate_cpu_core_gorvernor_paths(&epp_files) {
+        Ok(v) => v,
+        Err(e) => {
+            log::error!("{e}");
+            process::exit(1);
+        }
+    };
+    if epp_files.is_empty() {
+        log::error!("Could not find any valid governor files. Exiting.");
+        process::exit(1);
+    }
+    let config = match read_config() {
+        Ok(c) => c,
+        Err(e) => {
+            log::error!("{e}");
+            process::exit(1);
+        }
+    };
 
     let controller = EPPController {
-        core_files: epp_files,
+        epp_core_files: epp_files,
+        epp_config: config.epp,
+        governor_core_files: governor_files,
+        governor_config: config.scaling_governor,
     };
     loop {
         match controller.run() {
