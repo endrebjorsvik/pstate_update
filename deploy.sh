fdc8a0a8a9adbcbc4b67556b9ae923b84dc200b7
Endre Bjørsvik <endrebjorsvik@gmail.com>
2023-11-11 01:57:34 +0100

    Allow existing folder in deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 cargo build --release
 sudo cp target/release/pstate_update /usr/local/bin/
-sudo mkdir /etc/pstate_update
+sudo mkdir -p /etc/pstate_update
 sudo cp config.toml /etc/pstate_update/
 sudo cp pstate_update.service /etc/systemd/system/
 sudo systemctl daemon-reload
